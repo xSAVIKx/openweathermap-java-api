1a5197a5bd7c4cb0b1c8671d8f7a6481310ed258
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2018-01-14 22:15:02 +0200

    added test for forecast Uvi query

--- api-core/src/test/groovy/org/openweathermap/api/UrlConnectionWeatherClientSpockBetamaxTest.groovy
+++ api-core/src/test/groovy/org/openweathermap/api/UrlConnectionWeatherClientSpockBetamaxTest.groovy
@@ -11,7 +11,7 @@ import software.betamax.junit.RecorderRule
 import spock.lang.Specification
 
 class UrlConnectionWeatherClientSpockBetamaxTest extends Specification {
-    private static final String API_KEY = "e727e62532fa3bedf05392e295969719"
+    private static final String API_KEY = "API_KEY_VALUE"
 
     private static final String TAPES_STORAGE = "src/test/resources/org/openweathermap/api/tapes"
 
@@ -380,5 +380,34 @@ class UrlConnectionWeatherClientSpockBetamaxTest extends Specification {
         result.isoDate == '2018-01-14T12:00:00Z'
         result.date == new Date(1515931200000L)
         result.ultravioletIndex == 0.68d
+    }
+
+    @Betamax(tape = "by coordinates", mode = TapeMode.READ_WRITE)
+    def "should return Uvi forecast by coordinates"() {
+        given:
+        final def longitude = "36.25"
+        final def latitude = "50"
+        final def kharkivCoordinate = new Coordinate(longitude, latitude)
+        final def client = new UrlConnectionDataWeatherClient(API_KEY)
+        final def query = UviQuery.Forecast.byGeographicCoordinates(kharkivCoordinate)
+                .language(Language.ENGLISH)
+                .unitFormat(UnitFormat.METRIC)
+                .count(1)
+                .build()
+        when:
+        final def result = client.getUviForecast(query)
+        then:
+        result.size() == 2
+        result[0].longitude == longitude
+        result[0].latitude == latitude
+        result[0].isoDate == '2018-01-15T12:00:00Z'
+        result[0].date == new Date(1516017600000L)
+        result[0].ultravioletIndex == 0.65d
+
+        result[1].longitude == longitude
+        result[1].latitude == latitude
+        result[1].isoDate == '2018-01-16T12:00:00Z'
+        result[1].date == new Date(1516104000000L)
+        result[1].ultravioletIndex == 0.64d
     }
 }
