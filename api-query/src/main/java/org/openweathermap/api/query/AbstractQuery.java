ea2995afa607eaad40ff136010b88a87b5d0de76
Yurii Serhiichuk <savik.ne@gmail.com>
2021-06-20 17:33:51 +0300

    Add missing copyright.

--- api-query/src/main/java/org/openweathermap/api/query/AbstractQuery.java
+++ api-query/src/main/java/org/openweathermap/api/query/AbstractQuery.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2021, Yurii Serhiichuk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.openweathermap.api.query;
 
 import lombok.Data;
@@ -6,7 +23,7 @@ import lombok.Data;
 public abstract class AbstractQuery implements Query {
     private UnitFormat unitFormat;
     private Language language;
-    private String baseUrl = BASE_URL;
+    private final String baseUrl = BASE_URL;
 
     @Override
     public String toStringRepresentation(String apiKey) {
